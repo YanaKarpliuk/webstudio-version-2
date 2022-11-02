1f0e14d58a0a3b0d99367ea4c4a498482e7cf241
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-11-02 23:47:08 +0200

    hash router: fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
